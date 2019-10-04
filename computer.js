22a89371f4257e42fe3c97aa6bcb08a2d7d20ebf
Анастасия Лещенко <leshencko.nastia2013@gmail.com>
2019-10-04 14:48:42 +0300

    united to one js file

--- computer.js
+++ computer.js
@@ -22,3 +22,21 @@ class Computer {
         return [this.manufacturer, this.processor];
     }
 }
+
+class Ultrabook extends Computer {
+    constructor(manufacturer, processor, hardDiskSize) {
+        super(manufacturer, processor);
+        this._hardDiskSize = hardDiskSize;
+    }
+
+    get hardDiskSize() {
+        return this._hardDiskSize;
+    }
+    set hardDiskSize(newHardDiskSize) {
+        this._hardDiskSize = newHardDiskSize;
+    }
+
+    showInfo() {
+        return [super.showInfo(), this.hardDiskSize];
+    }
+}
